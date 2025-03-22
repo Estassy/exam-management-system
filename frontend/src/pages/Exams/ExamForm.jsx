6ac421a3d56b16ba3f529437363636e06697c129
estassy <marcbataba99@gmail.com>
2025-03-22 13:48:26 +0100

    add

--- frontend/src/pages/Exams/ExamForm.jsx
+++ frontend/src/pages/Exams/ExamForm.jsx
@@ -9,9 +9,21 @@ import "./ExamForm.scss"; // Importation du style
 import { getAllTeachers } from "../../services/user/userService";
 import { AuthContext } from "../../context/AuthContext";
 import { getAllCourses } from "../../services/course/courseService";
+import {
+  HomeIcon,
+  CalendarDaysIcon,
+  UsersIcon,
+  Cog6ToothIcon,
+  PencilSquareIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
+import Sidebar from "../../components/UI/Sidebar";
+import logo from "../../../src/assets/images/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const ExamForm = () => {
   const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
   const [templates, setTemplates] = useState([]);
   const [promotions, setPromotions] = useState([]);
   const [selectedTemplate, setSelectedTemplate] = useState("");
@@ -24,6 +36,38 @@ const ExamForm = () => {
   const [teachers, setTeachers] = useState([]);
   const [selectedTeacher, setSelectedTeacher] = useState(""); // ✅ Stocke l'enseignant sélectionné
   const [teacherId, setTeacherId] = useState("");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+
+  const teacherMenuItems = [
+    { label: "Accueil", icon: HomeIcon, onClick: () => navigate("/dashboard") },
+    {
+      label: "Cours",
+      icon: CalendarDaysIcon,
+      onClick: () => navigate("/courses"),
+    },
+    {
+      label: "Créer un cours",
+      icon: PlusCircleIcon,
+      onClick: () => navigate("/create-course"),
+    },
+    {
+      label: "Examens",
+      icon: CalendarDaysIcon,
+      onClick: () => navigate("/QuizExamsPage"),
+    },
+    {
+      label: "Étudiants",
+      icon: UsersIcon,
+      onClick: () => navigate("/students"),
+    },
+    {
+      label: "Notes",
+      icon: UsersIcon,
+      onClick: () => navigate("/grades"),
+    },
+  ];
 
   useEffect(() => {
     if (user && user.role === "TEACHER") {
@@ -146,97 +190,105 @@ const ExamForm = () => {
   };
 
   return (
-    <div className="exam-form">
-      <div className="exam-title">Créer un examen</div>
+    <div className={`dashboard-container ${isSidebarOpen ? "shifted" : ""}`}>
+      <Sidebar
+        isOpen={isSidebarOpen}
+        toggleSidebar={toggleSidebar}
+        logoSrc={logo}
+        menuItems={teacherMenuItems}
+      />
+      <div className="exam-form">
+        <div className="exam-title">Créer un examen</div>
 
-      <form onSubmit={handleSubmit}>
-        {/* Choix du modèle */}
-        <div className="form-group">
-          <label className="form-label">Utiliser un modèle :</label>
-          <select
-            className="form-input"
-            value={selectedTemplate}
-            onChange={(e) => setSelectedTemplate(e.target.value)}
-          >
-            <option value="">-- Sélectionnez un modèle --</option>
-            {templates.map((template) => (
-              <option key={template.id} value={template.id}>
-                {template.title}
-              </option>
-            ))}
-          </select>
-        </div>
+        <form onSubmit={handleSubmit}>
+          {/* Choix du modèle */}
+          <div className="form-group">
+            <label className="form-label">Utiliser un modèle :</label>
+            <select
+              className="form-input"
+              value={selectedTemplate}
+              onChange={(e) => setSelectedTemplate(e.target.value)}
+            >
+              <option value="">-- Sélectionnez un modèle --</option>
+              {templates.map((template) => (
+                <option key={template.id} value={template.id}>
+                  {template.title}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        {/* Choix de la promotion */}
-        <div className="form-group">
-          <label className="form-label">Associer à une promotion :</label>
-          <select
-            className="form-input"
-            value={selectedPromotion}
-            onChange={(e) => setSelectedPromotion(e.target.value)}
-          >
-            <option value="">-- Sélectionnez une promotion --</option>
-            {promotions.map((promotion) => (
-              <option key={promotion.id} value={promotion.id}>
-                {promotion.name}
-              </option>
-            ))}
-          </select>
-        </div>
+          {/* Choix de la promotion */}
+          <div className="form-group">
+            <label className="form-label">Associer à une promotion :</label>
+            <select
+              className="form-input"
+              value={selectedPromotion}
+              onChange={(e) => setSelectedPromotion(e.target.value)}
+            >
+              <option value="">-- Sélectionnez une promotion --</option>
+              {promotions.map((promotion) => (
+                <option key={promotion.id} value={promotion.id}>
+                  {promotion.name}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        {/* Sélection du cours */}
-        <div className="form-group">
-          <label className="form-label">Associer à un cours :</label>
-          <select
-            className="form-input"
-            value={selectedCourse}
-            onChange={(e) => setSelectedCourse(e.target.value)}
-            required
-          >
-            <option value="">-- Sélectionnez un cours --</option>
-            {courses.map((course) => (
-              <option key={course.id} value={course.id}>
-                {course.title}
-              </option>
-            ))}
-          </select>
-        </div>
+          {/* Sélection du cours */}
+          <div className="form-group">
+            <label className="form-label">Associer à un cours :</label>
+            <select
+              className="form-input"
+              value={selectedCourse}
+              onChange={(e) => setSelectedCourse(e.target.value)}
+              required
+            >
+              <option value="">-- Sélectionnez un cours --</option>
+              {courses.map((course) => (
+                <option key={course.id} value={course.id}>
+                  {course.title}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        {/* Saisie manuelle du titre */}
-        <div className="form-group">
-          <label className="form-label">Titre de l'examen :</label>
-          <input
-            className="form-input"
-            type="text"
-            value={examTitle}
-            onChange={(e) => setExamTitle(e.target.value)}
-            disabled={!!selectedTemplate} // Désactiver si un modèle est choisi
-          />
-        </div>
+          {/* Saisie manuelle du titre */}
+          <div className="form-group">
+            <label className="form-label">Titre de l'examen :</label>
+            <input
+              className="form-input"
+              type="text"
+              value={examTitle}
+              onChange={(e) => setExamTitle(e.target.value)}
+              disabled={!!selectedTemplate} // Désactiver si un modèle est choisi
+            />
+          </div>
 
-        {/* Sélection de la date */}
-        <div className="form-group">
-          <label className="form-label">Date de l'examen :</label>
-          <input
-            className="form-input"
-            type="datetime-local"
-            value={examDate}
-            onChange={(e) => setExamDate(e.target.value)}
-          />
-        </div>
+          {/* Sélection de la date */}
+          <div className="form-group">
+            <label className="form-label">Date de l'examen :</label>
+            <input
+              className="form-input"
+              type="datetime-local"
+              value={examDate}
+              onChange={(e) => setExamDate(e.target.value)}
+            />
+          </div>
 
-        {/* Bouton de soumission */}
-        <button type="submit" className="submit-btn">
-          Créer l'examen
-        </button>
+          {/* Bouton de soumission */}
+          <button type="submit" className="submit-btn">
+            Créer l'examen
+          </button>
 
-        {/* Affichage des messages d'erreur ou succès */}
-        {message && (
-          <p className={message.includes("✅") ? "success" : "error"}>
-            {message}
-          </p>
-        )}
-      </form>
+          {/* Affichage des messages d'erreur ou succès */}
+          {message && (
+            <p className={message.includes("✅") ? "success" : "error"}>
+              {message}
+            </p>
+          )}
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Quizzes/QuizForm.jsx
+++ frontend/src/pages/Quizzes/QuizForm.jsx
@@ -3,8 +3,20 @@ import "./QuizForm.scss";
 import { getAllQuizTemplates } from "../../services/quiz/quizTemplateService";
 import { getPromotions } from "../../services/promotion/promotionService";
 import { createQuiz } from "../../services/quiz/quizService";
+import {
+  HomeIcon,
+  CalendarDaysIcon,
+  UsersIcon,
+  Cog6ToothIcon,
+  PencilSquareIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
+import Sidebar from "../../components/UI/Sidebar";
+import logo from "../../../src/assets/images/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const QuizForm = () => {
+  const navigate = useNavigate();
   const [templates, setTemplates] = useState([]);
   const [promotions, setPromotions] = useState([]);
   const [selectedTemplate, setSelectedTemplate] = useState("");
@@ -12,6 +24,38 @@ const QuizForm = () => {
   const [quizTitle, setQuizTitle] = useState("");
   const [selectedQuestions, setSelectedQuestions] = useState([]);
   const [useTemplate, setUseTemplate] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+
+  const teacherMenuItems = [
+    { label: "Accueil", icon: HomeIcon, onClick: () => navigate("/dashboard") },
+    {
+      label: "Cours",
+      icon: CalendarDaysIcon,
+      onClick: () => navigate("/courses"),
+    },
+    {
+      label: "Créer un cours",
+      icon: PlusCircleIcon,
+      onClick: () => navigate("/create-course"),
+    },
+    {
+      label: "Examens",
+      icon: CalendarDaysIcon,
+      onClick: () => navigate("/QuizExamsPage"),
+    },
+    {
+      label: "Étudiants",
+      icon: UsersIcon,
+      onClick: () => navigate("/students"),
+    },
+    {
+      label: "Notes",
+      icon: UsersIcon,
+      onClick: () => navigate("/grades"),
+    },
+  ];
 
   useEffect(() => {
     async function fetchData() {
@@ -41,77 +85,85 @@ const QuizForm = () => {
   };
 
   return (
-    <div className="quiz-form">
-      <h2>Créer un Quiz</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Nom du Quiz</label>
-          <input
-            type="text"
-            value={quizTitle}
-            onChange={(e) => setQuizTitle(e.target.value)}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Choisir une promotion</label>
-          <select
-            value={selectedPromotion}
-            onChange={(e) => setSelectedPromotion(e.target.value)}
-          >
-            <option value="">-- Sélectionnez une promotion --</option>
-            {promotions.map((promo) => (
-              <option key={promo.id} value={promo.id}>
-                {promo.name}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label>
-            <input
-              type="checkbox"
-              checked={useTemplate}
-              onChange={() => setUseTemplate(!useTemplate)}
-            />
-            Utiliser un modèle existant
-          </label>
-        </div>
-
-        {useTemplate ? (
+    <div className={`dashboard-container ${isSidebarOpen ? "shifted" : ""}`}>
+      <Sidebar
+        isOpen={isSidebarOpen}
+        toggleSidebar={toggleSidebar}
+        logoSrc={logo}
+        menuItems={teacherMenuItems}
+      />
+      <div className="quiz-form">
+        <h2>Créer un Quiz</h2>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
-            <label>Choisir un modèle</label>
+            <label>Nom du Quiz</label>
+            <input
+              type="text"
+              value={quizTitle}
+              onChange={(e) => setQuizTitle(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Choisir une promotion</label>
             <select
-              value={selectedTemplate}
-              onChange={(e) => setSelectedTemplate(e.target.value)}
+              value={selectedPromotion}
+              onChange={(e) => setSelectedPromotion(e.target.value)}
             >
-              <option value="">-- Sélectionnez un modèle --</option>
-              {templates.map((template) => (
-                <option key={template.id} value={template.id}>
-                  {template.title}
+              <option value="">-- Sélectionnez une promotion --</option>
+              {promotions.map((promo) => (
+                <option key={promo.id} value={promo.id}>
+                  {promo.name}
                 </option>
               ))}
             </select>
           </div>
-        ) : (
-          <div className="form-group">
-            <label>Ajouter des questions manuellement</label>
-            <input
-              type="text"
-              placeholder="Ajoutez l'ID des questions (séparées par des virgules)"
-              onChange={(e) =>
-                setSelectedQuestions(
-                  e.target.value.split(",").map((id) => id.trim())
-                )
-              }
-            />
-          </div>
-        )}
 
-        <button type="submit">Créer le Quiz</button>
-      </form>
+          <div className="form-group">
+            <label>
+              <input
+                type="checkbox"
+                checked={useTemplate}
+                onChange={() => setUseTemplate(!useTemplate)}
+              />
+              Utiliser un modèle existant
+            </label>
+          </div>
+
+          {useTemplate ? (
+            <div className="form-group">
+              <label>Choisir un modèle</label>
+              <select
+                value={selectedTemplate}
+                onChange={(e) => setSelectedTemplate(e.target.value)}
+              >
+                <option value="">-- Sélectionnez un modèle --</option>
+                {templates.map((template) => (
+                  <option key={template.id} value={template.id}>
+                    {template.title}
+                  </option>
+                ))}
+              </select>
+            </div>
+          ) : (
+            <div className="form-group">
+              <label>Ajouter des questions manuellement</label>
+              <input
+                type="text"
+                placeholder="Ajoutez l'ID des questions (séparées par des virgules)"
+                onChange={(e) =>
+                  setSelectedQuestions(
+                    e.target.value.split(",").map((id) => id.trim())
+                  )
+                }
+              />
+            </div>
+          )}
+
+          <button type="submit">Créer le Quiz</button>
+        </form>
+      </div>
     </div>
   );
 };
